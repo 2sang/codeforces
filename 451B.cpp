77ca163de2310d31ace22933a2d44a9bec2774b7
2sang <sangsulee92@gmail.com>
2018-10-01 00:21:58 +0900

    Add 451B

--- 451B.cpp
+++ 451B.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(int argc, char *argv[]){
-    long long a[100001];
+    long long a[100001], sorted[100001];
     long long n;
     cin >> n;
     for (long long i = 0; i < n; i++) {
         long long x;
         cin >> x;
-        a[i] = x;
+        a[i] = x; sorted[i] = x;
     }
-    long long start=0, end=0;
-    bool done=false;
-    for (long long i = 0; i < n-1; i++) {
-        if (!done & a[i] < a[i+1]) {
-            end++;
-        } else if (!done & a[i] >= a[i+1]) {
-            done = true;
-            
-        }
+    sort(sorted, sorted+n);
+    if (equal(a, a+n, sorted)) {
+        cout << "yes" << endl;
+        cout << "1 1" << endl;
+        return 0;
     }
     
+    long long lp=0, rp=n-1;
+    bool ldone=false, rdone=false;
+    while (lp <= rp) {
+        if (ldone && rdone) break;
+        
+        if (a[lp] == sorted[lp]) lp++;
+        else ldone=true;
+        
+        if (a[rp] == sorted[rp]) rp--;
+        else rdone=true;
+    }
+    reverse(a+lp, a+rp+1);
+    if (equal(a, a+n, sorted)) {
+        cout << "yes" << endl;
+        cout << lp+1 << " " << rp+1 << endl;
+    } else {
+        cout << "no" << endl;
+    }
     return 0;
 }
