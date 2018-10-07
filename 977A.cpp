191c9d132751aae30e987a0cc40a37d9cba54ef5
2sang <sangsulee92@gmail.com>
2018-10-07 16:41:23 +0900

    Now add some template with snippets

--- 977A.cpp
+++ 977A.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+int solve();
 int main(int argc, char *argv[]){
+    bool submission=true;
+    if (submission) {solve(); return 0;}
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    int tc; cin >> tc; while (tc--) {solve();}
+}
+
+int solve() {
     long long n; int k;
     cin >> n >> k;
     while (k--) {
